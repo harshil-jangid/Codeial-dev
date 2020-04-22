abee4963ce4505da9311174585c4bc6982c2b93b
Harshil Jangid <harshil.jangid90@gmail.com>
2020-04-22 17:09:26 +0530

    Added sign in and sign up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,26 @@ module.exports.profile = function(req, res){
         title: 'User Profile'
     })
 }
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'Sign up'
+    })
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'Sign in'
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
+    // TODO later
+}
+
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    // TODO later
+}
