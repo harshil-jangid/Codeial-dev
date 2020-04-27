8ae2cf54ccde34b81924940705bf8f9772127fa6
Harshil Jangid <harshil.jangid90@gmail.com>
2020-04-27 23:34:28 +0530

    Converting our code to AJAX(getting the post request only)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,15 +3,25 @@ const Comment=require('../models/comments');
 
 module.exports.create = async function(req, res){
     try {
-        await Post.create({
+        let post=await Post.create({
             content: req.body.content,
             user: req.user._id
         });
 
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post:post
+                },
+                message:"Post Created!!"                
+            });
+        }
+
+        req.flash('success',"Post published");
         return res.redirect('back');
     } catch (error) {
-        console.log("Error",error);
-        return;
+        req.flash('error',err)
+        return res.redirect('back');
     }
 }
 
@@ -22,14 +32,16 @@ module.exports.destroy= async function(req,res){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id})
+            req.flash('success',"Post and associated comments deleted");
+            
             return res.redirect('back');
         }
         else{
             return res.redirect('back');
         }
     } catch (error) {
-        console.log("Error",error);
-        return;
+        req.flash('error',"You cannot delete this post");
+        return res.redirect('back');;
     }
     
 }
