b20e5d52cbbe84fc0144a57db386090e7ea237a5
Harshil Jangid <harshil.jangid90@gmail.com>
2020-04-30 20:14:58 +0530

    Converted Adding/Deleting comments to AJAX and added noty to them

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -14,6 +14,18 @@
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
                     deletePost($(' .delete-post-button', newPost))
+
+                    // call the create comment class
+                    new PostComments(data.data.post._id);
+
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post published!",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -67,6 +79,14 @@
                 url:$(deleteLink).prop('href'),
                 success: function(data){
                     $(`#post-${data.data.post_id}`).remove();
+                    new Noty({
+                        theme: 'relax',
+                        text: "Post Deleted",
+                        type: 'success',
+                        layout: 'topRight',
+                        timeout: 1500
+                        
+                    }).show();
                 },
                 error:function(error){
                     console.log(error.responseText);
@@ -74,9 +94,20 @@
             })
         })
     }
+    let convertPostsToAjax = function(){
+        $('#posts-list-container>ul>li').each(function(){
+            let self = $(this);
+            let deleteButton = $(' .delete-post-button', self);
+            deletePost(deleteButton);
 
+            // get the post's id by splitting the id attribute
+            let postId = self.prop('id').split("-")[1]
+            new PostComments(postId);
+        });
+    }
 
 
 
     createPost();
+    convertPostsToAjax();
 }
