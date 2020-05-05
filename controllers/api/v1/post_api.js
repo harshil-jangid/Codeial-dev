cdcdf6a733303d02271e121fb7b5cc46208c6909
Harshil Jangid <harshil.jangid90@gmail.com>
2020-05-05 22:53:24 +0530

    Started using passport-jwt and set up passport-jwt-strategy

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -1,7 +1,44 @@
-module.exports.index=function(req,res){
+const Post=require('../../../models/post');
+const Comment=require('../../../models/comments')
+
+module.exports.index=async function(req,res){
+
+    let posts= await Post.find({})
+                    .populate('user')
+                    .sort('-createdAt')
+                    .populate({
+                        path:'comments',
+                        populate:{
+                            path:'user'
+                        }
+                    });
+
     return res.json({
-        message:"List of posts",
-        posts:[]
+        message:"List of posts v1",
+        posts:posts
     })
+}
+
+module.exports.destroy= async function(req,res){
+    try {
+        let post= await Post.findById(req.params.id);
+        // if(post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id})
+            
+            return res.json(200,{
+                message:'Post and associated comments deleted successfully'
+            });
+        // }
+        // else{
+        //     return res.redirect('back');
+        // }
+    } catch (error) {
+        return res.json(500,{
+            message:"Internal server error"
+        })
+    }
+    
 }
 
--- routes/api/v2/posts.js
+++ routes/api/v2/posts.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const postApi = require("../../../controllers/api/v1/post_api")
+const postApi = require("../../../controllers/api/v2/post_api")
 
 
 router.get('/',postApi.index)
-router.get('/:id',postApi.destroy);
 module.exports=router;
